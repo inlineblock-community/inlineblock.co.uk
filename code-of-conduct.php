33c012d0e95af10dcd2d6a28fd8b6c9add8661ef
Marc Jenkins <marc@foxsoft.co.uk>
2017-01-02 11:23:09 +0000

    Add code of conduct page

---
+++ code-of-conduct.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title>inline-block Slack Community</title>
+	<meta name="description" content="">
+	<link rel="shortcut icon" href="favicon.png" />
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="stylesheet" href="assets/css/styles.css">
+</head>
+<body>
+
+<div class="wrapper">
+	<h1><a href="/"><img src="assets/images/slack.png"> inline-block</a></h1>
+	
+	<h2>Code of conduct</h2>
+	
+	<h3>In a nutshell</h3>
+	<p>Be respectful of other people, respectfully ask people to stop if you are bothered, and if you can't resolve an issue contact an admin. If you are being a problem, it will be apparent and you may be asked to leave.</p>
+	
+	<h3>Respect</h3>
+	<p>inline-block is an intentionally positive community that recognises and celebrates the creativity and collaboration of independent members and the diversity of skills, talents, experiences, cultures, and opinions that they bring to inline-block.</p>
+	<p>inline-block is an inclusive environment, based on treating all individuals respectfully, regardless of gender (including transgender status), sexual orientation, age, disability, medical conditions, nationality, ethnicity, religion (or lack thereof), physical appearance, politics, ideology (real world or tech), or software preferences.</p>
+	<p>We value respectful behavior above individual opinions.</p>
+	<p>Respectful behavior includes:</p>
+	<ul>
+		<li>Be considerate, kind, constructive, and helpful.</li>
+		<li>Avoid demeaning, discriminatory, harassing, hateful, or physically threatening behavior, speech, and imagery.</li>
+		<li>If you're not sure, ask someone instead of assuming.</li>
+	</ul>
+
+	<h3>Resolve peacefully</h3>
+	<p>We believe peer to peer discussions, feedback, and corrections can help build a stronger, safer, and more welcoming community.</p>
+	<p>If you see someone behaving disrespectfully, you are encouraged to respectfully discourage them from such behavior. Expect that others in the community wish to help keep the community respectful and welcome your input in doing so.</p>
+	<p>If you experience disrespectful behavior and feel in any way unable to respond or resolve it respectfully (for any reason), please immediately bring it to the attention of an admin. We want to hear from you about anything that you feel is disrespectful, threatening, or just icky in any way. We will listen and work to resolve the matter.</p>
+
+	<h3>Apologise for mistakes</h3>
+	<p>Should you catch yourself behaving disrespectfully, or be confronted as such, own up to your words and actions, and apologise accordingly. No one is perfect, and even well-intentioned people make mistakes. What matters is how you handle them, and avoiding repeating them in the future.</p>
+
+	<h3>Consequences</h3>
+	<p>If the administrators determine that a human is behaving disrespectfully, the administrators may take any action they deem appropriate, up to and including expulsion and exclusion from the community without warning.</p>
+	<p>As administrators, we will seek to resolve conflicts peacefully and in a manner that is positive for the community. We can't foresee every situation however, and thus if in the administrators' judgment the best thing to do is to ask a disrespectful individual to leave, we will do so.</p>
+
+	<h3>Administrators</h3>
+	<p>The administrators of inline-block as of October 17th, 2016:</p>
+	<ul>
+		<li>Alex Hardy - @alexhardy on Slack</li>
+		<li>Andy Henson - @andyh on Slack</li>
+		<li>Charles Roper - @charles on Slack</li>
+		<li>Darren Beale - @bealers on Slack</li>
+		<li>Dave Redfern - @daveredfern on Slack</li>
+		<li>Marc Jenkins - @marcjenkins on Slack</li>
+	</ul>
+
+	<h3>Thanks</h3>
+	<p>Special thanks to the IndieWebCamp for creating and sharing the <a href="https://indieweb.org/code-of-conduct">Code of Conduct</a> on which this one is based.</p>
+
+</div>
+
+</body>
+</html>
